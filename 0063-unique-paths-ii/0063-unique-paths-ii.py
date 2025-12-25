b10f7f4dcadae47bd72ff5725a2eae5d518ab96e
Ritvik Prakash <87536301+ritz10001@users.noreply.github.com>
2025-12-25 13:16:23 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -5,30 +5,18 @@ class Solution(object):
             return 0
         r = len(obstacleGrid)
         c = len(obstacleGrid[0])
-        dp = copy.deepcopy(obstacleGrid)
-        for i in range(c):
-            if dp[0][i] == 1:
-                dp[0][i] = "0"
-                break
-            dp[0][i] = "1"
-        for i in range(r):
-            if dp[i][0] == 1:
-                dp[i][0] = "0"
-                break
-            dp[i][0] = "1"
+        dp = [[0 for j in range(c)] for i in range(c)]
+        if obstacleGrid[0][0] == 0:
+            dp[0][0] = 1
         for i in range(r):
             for j in range(c):
-                if dp[i][j] == 1:
-                    dp[i][j] = "0"
+                if obstacleGrid[i][j] == 1:
+                    dp[i][j] = 0
                     continue
-                if dp[i][j] == "0":
-                    continue
-                total = 0
                 if i > 0:
-                    total += int(dp[i-1][j])
+                    dp[i][j] += dp[i-1][j]
                 if j > 0:
-                    total += int(dp[i][j-1])
-                dp[i][j] = max(int(dp[i][j]), total)
-        return int(dp[r-1][c-1])
-            
+                    dp[i][j] += dp[i][j-1]
+        return dp[r-1][c-1]
+                    
             
