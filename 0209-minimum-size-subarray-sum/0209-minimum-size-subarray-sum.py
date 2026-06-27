3448379f5c367b9096f2e7a02903da0cf16f2800
Ritvik Prakash <87536301+ritz10001@users.noreply.github.com>
2026-06-27 12:38:09 -0500

    Time: 23 ms (19.32%), Space: 20.2 MB (13.94%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -1,23 +1,14 @@
 class Solution(object):
-    """
-    idea: sliding window. Keep a L and R pointer. Start with L, R = 0, 0
-    if the sum of a specific window is less than target, increase R. If its
-    greater or equal, set the currentMin count accordingly. If otherwise,
-    increase L.
-    [2,3,1,2,4,2] -> 2 + 3 + 1 + 2 = 8 -> [L:R+1]
-     ^     ^
-     L     R
-    [2,3,1,2,4,2] -> 3 + 1 + 2 + 4 = 10
-       ^     ^
-    """
     def minSubArrayLen(self, target, nums):
-        currentSum = 0
         l = 0
-        minLen = float("inf")
+        res = float("inf")
+        currentSum = 0
         for r in range(len(nums)):
             currentSum += nums[r]
             while currentSum >= target:
-                minLen = min(minLen, r-l+1)
+                res = min(res, r - l + 1)
                 currentSum -= nums[l]
                 l += 1
-        return minLen if minLen < float("inf") else 0
+        if res == float("inf"):
+            return 0
+        return res
