2481779c659cad6325b33bb9e3c57a54cc337f08
Ritvik Prakash <87536301+ritz10001@users.noreply.github.com>
2026-06-27 18:28:07 -0500

    Time: 4 ms (19.75%), Space: 12.5 MB (21.18%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.py
+++ 0289-game-of-life/0289-game-of-life.py
@@ -1,23 +1,26 @@
 import copy
 class Solution(object):
     def gameOfLife(self, board):
-        boardCopy = copy.deepcopy(board)
         offsets = [(0,1),(1,0),(1,1),(-1,-1),(-1,0),(0,-1), (-1,1),(1,-1)]
-        r = len(boardCopy)
-        c = len(boardCopy[0])
+        r = len(board)
+        c = len(board[0])
         for i in range(r):
             for j in range(c):
-                current_cell = boardCopy[i][j]
+                current_cell = board[i][j]
                 live_neighbors = 0
                 for x, y in offsets:
                     offset_x = x + i
                     offset_y = y + j
                     if 0 <= offset_x < r and 0 <= offset_y < c: # live cell
-                        live_neighbors += boardCopy[offset_x][offset_y]
+                        if board[offset_x][offset_y] in (1,2):
+                            live_neighbors += 1
                 if current_cell == 1:
-                    if live_neighbors not in (2,3):
-                        board[i][j] = 0 
+                    if live_neighbors not in (2,3): 
+                        board[i][j] = 2 # live to dead
                 else:
                     if live_neighbors == 3:
-                        board[i][j] = 1
+                        board[i][j] = 3 # dead to live
+        for i in range(r):
+            for j in range(c):
+                board[i][j] %= 2
         
