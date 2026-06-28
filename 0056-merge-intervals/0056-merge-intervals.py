a923c75c1acdc505cdbba91ba06fc821f923f9d2
Ritvik Prakash <87536301+ritz10001@users.noreply.github.com>
2026-06-28 16:57:36 -0500

    Time: 12 ms (38.12%), Space: 15.9 MB (73.67%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -6,7 +6,7 @@ class Solution(object):
         res = [intervals[0]]
         for i in range(1, len(intervals)):
             if intervals[i][0] <= res[-1][1]: # mergeable
-                res[-1] = [min(res[-1][0], intervals[i][0]), max(res[-1][1], intervals[i][1])]
+                res[-1] = [res[-1][0], max(res[-1][1], intervals[i][1])]
             else:
                 res.append(intervals[i])
         return res
