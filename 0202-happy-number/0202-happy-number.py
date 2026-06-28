0b5a133de5b899248466ef37350c666d5da253e6
Ritvik Prakash <87536301+ritz10001@users.noreply.github.com>
2026-06-28 12:56:27 -0500

    Time: 2 ms (57.18%), Space: 12.3 MB (88.88%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,14 +1,11 @@
 class Solution(object):
     def isHappy(self, n):
         numbers = set()
-        while n not in numbers:
+        while n != 1 and n not in numbers:
             numbers.add(n)
             sumx = 0
             while n > 0:
-                digit = n % 10
-                sumx += digit ** 2
+                sumx += (n % 10) ** 2
                 n //= 10
-            if sumx == 1:
-                return True
             n = sumx
-        return False
+        return n == 1
