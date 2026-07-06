f284ff578c00034182dcc9482d52a6e62d4d94cf
Ritvik Prakash <87536301+ritz10001@users.noreply.github.com>
2026-07-05 21:50:02 -0500

    Time: 5 ms (16.36%), Space: 17.5 MB (6.49%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -4,16 +4,21 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
 class Solution(object):
-    def isValidBST(self, root):
-        queue = deque([(root, -float("inf"), float("inf"))])
-        while queue:
-            node, left, right = queue.popleft()
-            if not left < node.val < right:
-                return False
-            if node.left:
-                queue.append([node.left, left, node.val])
-            if node.right:
-                queue.append([node.right, node.val, right])
-        return True
+    
+        def isValidBST(self, root):
+            self.arr = []
+            def inorder(root):
+                if not root:
+                    return
+                inorder(root.left)
+                self.arr.append(root.val)
+                inorder(root.right)
+
+            inorder(root)
+
+            for i in range(1, len(self.arr)):
+                if self.arr[i] <= self.arr[i-1]:
+                    return False
+            return True
+            
